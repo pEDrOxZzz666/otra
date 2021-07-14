5c8ff5c0b2746c33fe71cf0a9266b443d8f6ce35
Pedro <pedroxzzz@gmail.com>
2021-07-14 18:39:22 +0200

    prueba guardado

---
+++ src/app/empleado/empleado.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-empleado',
+  templateUrl: './empleado.component.html',
+  styleUrls: ['./empleado.component.css']
+})
+export class EmpleadoComponent implements OnInit {
+
+  nombre = "Pedro";
+  apellido = "Ruiz";
+  edad = 17;
+  //empresa = "Píldoras Informaticas";
+
+  /*getEdad() {
+    return this.edad;
+  }*/
+
+  //propertyBindings
+  habilitacionCuadro = false;
+
+  registrado = false;
+
+  textoRegistro = "No hay nadie registrado";
+
+  getRegistroUusuario() {
+    this.registrado = false;
+  }
+
+  setusuarioRegistrado(event:Event) {     
+      if((<HTMLInputElement>event.target).value == "si") {
+        this.textoRegistro = "Usuario registrado";  
+      } 
+      else {
+        this.textoRegistro = "No hay nadie registrado"; 
+      }
+
+  }
+
+  prueba = "";
+
+  eventoPrueba(event:Event) {    
+    if((<HTMLInputElement>event.target).checked) {
+      this.prueba = (<HTMLInputElement>event.target).value;      
+    } else {
+      this.prueba = "";
+    }
+  }
+
+  getValue(event: Event): string {
+    return (event.target as HTMLInputElement).value;
+  }
+
+
+  //FIN PROPERTYBINDINGS
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
